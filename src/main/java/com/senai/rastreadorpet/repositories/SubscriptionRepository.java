815fd8f7a3742315c8eccb569187de6b8c380389
João <l2mortal01@gmail.com>
2025-09-18 16:18:03 -0300

    Update

--- src/main/java/com/senai/rastreadorpet/repositories/SubscriptionRepository.java
+++ src/main/java/com/senai/rastreadorpet/repositories/SubscriptionRepository.java
@@ -1,9 +1,9 @@
 package com.senai.rastreadorpet.repositories;
 
-import com.senai.rastreadorpet.models.Assinatura;
+import com.senai.rastreadorpet.models.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AssinaturaRepository extends JpaRepository<Assinatura, Integer> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
 }
