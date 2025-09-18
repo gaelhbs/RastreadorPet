1f6917d30e888969c06c8086353350a35128bd3f
João <l2mortal01@gmail.com>
2025-09-18 18:37:35 -0300

    Receipt - Repository

---
+++ src/main/java/com/senai/rastreadorpet/repositories/ReceiptRepository.java
@@ -0,0 +1,9 @@
+package com.senai.rastreadorpet.repositories;
+
+import com.senai.rastreadorpet.models.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
+}
