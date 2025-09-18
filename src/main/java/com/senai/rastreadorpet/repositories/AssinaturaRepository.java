2e763a9deda10fb783e980ddbe51e680ad9e04c7
João <l2mortal01@gmail.com>
2025-09-18 15:58:42 -0300

    Assinatura - Repository

---
+++ src/main/java/com/senai/rastreadorpet/repositories/AssinaturaRepository.java
@@ -0,0 +1,9 @@
+package com.senai.rastreadorpet.repositories;
+
+import com.senai.rastreadorpet.models.Assinatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssinaturaRepository extends JpaRepository<Assinatura, Integer> {
+}
