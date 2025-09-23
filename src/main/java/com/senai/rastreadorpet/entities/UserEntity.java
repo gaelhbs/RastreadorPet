6426509be55245b9953d27146134945952bf7ed5
gaelhbs <gabrielsouza1170@gmail.com>
2025-09-23 17:24:55 -0300

    Exemplo para atividade Jefte

---
+++ src/main/java/com/senai/rastreadorpet/entities/UserEntity.java
@@ -0,0 +1,30 @@
+package com.senai.rastreadorpet.entities;
+
+import com.senai.rastreadorpet.models.enums.CustomerRole;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+@Setter
+public class UserEntity {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String postalCode;
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private LocalDateTime registrationDate;
+    private CustomerRole role;
+    private LocalDateTime privacyConsentDate;
+    private String preferences;
+}
