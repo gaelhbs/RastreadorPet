171bedda37a6b194011995ff6e0d616c323d8d11
gaelhbs <gabrielsouza1170@gmail.com>
2025-09-25 00:13:49 -0300

    Adiciona CRUD de PET

--- src/main/java/com/senai/rastreadorpet/controllers/PetController.java
+++ src/main/java/com/senai/rastreadorpet/controllers/PetController.java
@@ -1,7 +1,7 @@
 package com.senai.rastreadorpet.controllers;
 
+import com.senai.rastreadorpet.entities.PetEntity;
 import com.senai.rastreadorpet.facade.PetFacade;
-import com.senai.rastreadorpet.models.Pet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,15 +17,33 @@ public class PetController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Pet> getAllPets() {
-        List<Pet> pets = petFacade.findAll();
+    public List<PetEntity> getAllPets() {
+        List<PetEntity> pets = petFacade.findAll();
         return petFacade.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Pet createUser(@RequestBody Pet pet) {
-        return petFacade.save(pet);
+    public PetEntity createUser(@RequestBody PetEntity pet) {
+        return petFacade.create(pet);
 
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PetEntity getUserById(@PathVariable int id) {
+        return petFacade.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PetEntity updateUser(@PathVariable int id, @RequestBody PetEntity pet) {
+        return petFacade.update(id, pet);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id) {
+        petFacade.delete(id);
     }
 }
